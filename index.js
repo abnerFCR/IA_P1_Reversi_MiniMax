c0865de1f069dc0698a607723d3f70237051e9ce
Abner Fernando Cardona <abner.cardona1997@gmail.com>
2021-06-24 08:30:57 -0600

    Prueba tablero

--- index.js
+++ index.js
@@ -4,18 +4,43 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    //1 -> blanco (Jugador 1) -> 2
-    //0 -> negro  (Jugador 2) -> 1
-    //2 -> espacio blanco     -> 0
+    //1 -> blanco (Jugador 1) -> 2    - 1    
+    //0 -> negro  (Jugador 2) -> 1    - 2 
+    //2 -> espacio blanco     -> 0       
     console.log(req.url);
     let url = req.url.toString().substring(2,req.url.length);
     let partes = url.split('&');
     let turno = partes[0].substring(6,partes[0].length);
     let estado = partes[1].substring(7,partes[1].length);
-    console.log('turno:', turno);
-    console.log('estado:', estado);
 
+    if(turno == '1'){
+        turnoN = 1;
+    }else{
+        turnoN = 2;
+    }
+    let posicion = 0;
+    let tablero=[];
+    for(let i = 0; i<8; i++){
+        let fila = [];
+        for(let j = 0; j<8; j++){
+            if(estado[posicion] == '1'){
+                fila.push(1);
+            }else if(estado[posicion=='2']){
+                fila.push(0);
+            }else{
+                fila.push(2);
+            }
+            posicion++;
+        }
+        tablero.push(fila);
+    }
     //Minimax.resolver();
+    console.log(turno);
+    for(let i = 0; i<8; i++){
+        for(let j = 0; j<8; j++){
+            console.log(tablero[i][j]);
+        }
+    }
 
     res.send('24');
 })
